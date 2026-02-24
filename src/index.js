816b330091a975053bc92334bd9e93547e9992ee
Vidhyadhran-03 <rvdharan31@gmail.com>
2026-02-24 20:54:00 +0530

    feat: overhaul system architecture
    BREAKING CHANGE: this update migrates the database schema and is not backward compatible

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 console.log("auth-service running this time sure");
 console.log("Done Architecture change");
 console.log("Not working major now trying minor ");
+
+console.log("Again trying major update");
