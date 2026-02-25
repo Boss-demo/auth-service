9217a119cd82edf028170428e9570632cc79df27
Lokesh Kumar <lokesh22ari@gmail.com>
2026-02-26 00:45:10 +0530

    feat: test tier release

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ console.log("Done Architecture change");
 console.log("Not working major now trying minor ");
 
 console.log("Again trying major update");
+
+console.log("tier tagging test --> By ALK");
